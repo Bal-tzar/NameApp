247815d2fb197c9ee6cf9e6416d9cf10990ece23
Bal-tzar <ottodemare1@gmail.com>
2025-09-23 10:44:48 +0200

    fixed Name.cs

--- Models/Name.cs
+++ Models/Name.cs
@@ -1,4 +1,16 @@
-int Id { get; set; }
-string FirstName { get; set; }
-string LastName { get; set; }
-string FullName { get { return $"{FirstName} {LastName}"; } }
+using System.ComponentModel.DataAnnotations;
+
+namespace YourAppName.Models
+{
+    public class Name
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "Name must be between 1 and 100 characters")]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; } = string.Empty;
+        
+        public DateTime DateAdded { get; set; } = DateTime.UtcNow;
+    }
+}
